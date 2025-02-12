8e7618ad4284900acf0a1658a23313170be9f245
JeronimoLa <81096816+JeronimoLa@users.noreply.github.com>
2025-02-11 23:02:48 -0600

    improve daily data pulls

--- configuraton.py
+++ configuraton.py
@@ -3,4 +3,8 @@ from dotenv import dotenv_values
 CONFIG = dotenv_values(".env")  
 BASE_URL = "https://api.ouraring.com"
 OURA_FIRST_DAY = "2024-12-27"
-RING_CONFIG_URL = "/v2/sandbox/usercollection/ring_configuration"
+SANDBOX_RING_CONFIG_URL_SANDBOX = "/v2/sandbox/usercollection/ring_configuration"
+SANDBOX_URL_PATTERN = r'\/v2\/sandbox\/usercollection\/[^\s/{}]+'
+RING_CONFIG_URL = "/v2/usercollection/ring_configuration"
+URL_PATTERN = r'\/v2\/usercollection\/[^\s/{}]+'
+# OURA_FIRST_DAY = "2025-01-20"
--- health_data_processor.py
+++ health_data_processor.py
@@ -1,69 +1,80 @@
-import csv, json, requests, time, os, re
-from configuraton import BASE_URL, CONFIG, OURA_FIRST_DAY, RING_CONFIG_URL
+import json, re, requests
+from datetime import date, timedelta
+
+from configuraton import BASE_URL, CONFIG, OURA_FIRST_DAY, RING_CONFIG_URL, URL_PATTERN
 from helper import recursive_fun, write_csv_from_dicts
 
 
-def wrapper(method:str, endpoint:str, **kwargs) -> requests:
+def wrapper(method:str, endpoint:str, **kwargs) -> json:
     """ Basic api wrapper """
-    try: 
-	    kwargs["params"]
+    try:
+        kwargs["params"]
     except KeyError:
-	    pass
+        pass
 
-    headers = { 'Authorization': f'Bearer {CONFIG["token"]}' }   
+    headers = {'Authorization': f'Bearer {CONFIG["token"]}'}
     return requests.request(method, BASE_URL + endpoint, headers=headers).json()
 
-def api_specification() -> dict:
-	""" Returns the openapi spec in a dict object """
-	with open("docs/json/openapi-1.23.json", "r+") as file:
-		return json.loads(file.read())
+def api_specification() -> dict:  
+    """ Returns the openapi spec in a dict object """
+    with open("docs/json/openapi-1.23.json", "r+", encoding="utf-8") as file:
+        return json.loads(file.read())
 
 def extract_unique_urls() -> set:
     data = api_specification()
-    pattern = r'\/v2\/sandbox\/usercollection\/[^\s/{}]+'
-    urls = set(re.search(pattern, path).group() for path in recursive_fun(data["paths"]) if re.search(pattern, path))
+    urls = set(re.search(URL_PATTERN, path).group() for path in recursive_fun(data["paths"]) if re.search(URL_PATTERN, path))
     urls.remove(RING_CONFIG_URL)
     return urls
 
-def load_tracker(fieldnames=None) -> dict:
+def load_tracker(fieldnames=None):
     try:
-        with open("data_tracker.json", "r+") as file:
+        with open("data_tracker.json", "r+", encoding="utf-8") as file:
             return json.loads(file.read())
     except FileNotFoundError:
         data = {file: {} for file in fieldnames}
-        with open("data_tracker.json", "w+") as file:
+        with open("data_tracker.json", "w+", encoding="utf-8") as file:
             file.write(json.dumps(data, indent=4))
     
 def save_tracker(updated_data):
-    with open("data_tracker.json", "w+") as file:
+    with open("data_tracker.json", "w+", encoding="utf-8") as file:
         file.write(json.dumps(updated_data, indent=4))
 
 def initial_data_pull():
-    url_to_filename = { file: re.search(r'[^/]+$', file).group() for file in extract_unique_urls() }
+    url_to_filename = {file: re.search(r'[^/]+$', file).group() for file in extract_unique_urls()}
     tracker = load_tracker(url_to_filename.values())
     for url, filename in url_to_filename.items():
         print(f"{url}start_date?{OURA_FIRST_DAY}")
-        tracker[filename]["endpoint"] = url 
-        tracker[filename]["last_indexed"] = OURA_FIRST_DAY
-        res = wrapper("GET", f"{url}?start_date={OURA_FIRST_DAY}")
-        keys = res["data"][0].keys()
-        write_csv_from_dicts(res["data"], keys, f"docs/csv/test/{filename}.csv")
-
-    save_tracker(tracker, OURA_FIRST_DAY)
-
-def process_new_data():
-    from datetime import date, timedelta, datetime
-
-    tracker = load_tracker()
-    last_indexed = tracker["sleep_time"]["last_indexed"]
-    year, month , day = map(int, last_indexed.split("-"))
-    date_obj = date(year, month, day)
-    date_to_collect_from = date_obj + timedelta(1)
-    endpoint = tracker["sleep_time"]["endpoint"]
-    res = wrapper("GET", f"{endpoint}?start_date={str(date_to_collect_from)}&end_date={date.today()-timedelta(1)}")
-    tracker["sleep_time"]["last_indexed"] = str(date_to_collect_from)
+        tracker[filename]["endpoint"] = url
+        tracker[filename]["last_indexed"] = str(date.today()-timedelta(2))
+        formatted_url = f"{url}?start_date={OURA_FIRST_DAY}&end_date={str(date.today()-timedelta(2))}"
+        try:
+            res = wrapper("GET", formatted_url)
+            keys = res["data"][0].keys()
+            write_csv_from_dicts(res["data"], keys, f"docs/csv/{filename}.csv")
+        except Exception as e:
+            print(f"An error occurred: no data exists at {url}")
     save_tracker(tracker)
 
+def process_new_data():
+    tracker = load_tracker()
+    for key, value in tracker.items():
+        last_indexed = tracker[key]["last_indexed"]
+        year, month, day = map(int, last_indexed.split("-"))
+        date_obj = date(year, month, day)
+        date_to_collect_from = date_obj + timedelta(1)
+        endpoint = tracker[key]["endpoint"]
+        try:
+            res = wrapper("GET", f"{endpoint}?start_date={str(date_obj)}&end_date={str(date_to_collect_from)}")
+            data_keys = res["data"][0].keys()
+            file_path = f"docs/csv/test/{key}.csv"
+            write_csv_from_dicts(res["data"], data_keys, file_path)
+            tracker[key]["last_indexed"] = str(date_to_collect_from)
+        except Exception as e:
+            print(e)
+            tracker[key]["last_indexed"] = str(date_obj)
+    save_tracker(tracker)
 
 if __name__ == "__main__":
-    process_new_data()
+    initial_data_pull()
+    # process_new_data()
+	# print(extract_unique_urls())
